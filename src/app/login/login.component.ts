16d9161eee8c241083dd31089eea5267fbef7b82
Austin Verburg <dairyboycomics@gmail.com>
2018-01-22 19:11:15 -0800

    auth0 installed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './../auth/auth.service';
 
 @Component({
   selector: 'fb-login',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  constructor(public auth: AuthService) {
+    auth.handleAuthentication();
+  }
   
 }
