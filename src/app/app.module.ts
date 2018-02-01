01fc098a291430aae753938952d31e8fedbd19a8
Austin Verburg <dairyboycomics@gmail.com>
2018-01-31 18:57:48 -0800

    login component functions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RoutingModule } from './app.routes';
+import { AUTH_PROVIDERS } from 'angular2-jwt';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavComponent } from './components/nav/nav.component';
 import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome';
 
+import { AuthService } from './services/auth.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-  
+    NavComponent
   ],
   imports: [
     BrowserModule,
     RoutingModule,
     Angular2FontawesomeModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    AUTH_PROVIDERS,
+    AuthService
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
