15f07b0f2d2b35e3cb6b70b21092fe1c8c1fedaa
Austin Verburg <dairyboycomics@gmail.com>
2018-01-31 19:43:52 -0800

    profile component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,9 @@ import { AUTH_PROVIDERS } from 'angular2-jwt';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
-import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome';
+import { ProfileComponent } from './components/profile/profile.component';
 
+import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome';
 import { AuthService } from './services/auth.service';
 
 
@@ -15,7 +16,8 @@ import { AuthService } from './services/auth.service';
   declarations: [
     AppComponent,
     HomeComponent,
-    NavComponent
+    NavComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -2,16 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from './../../services/auth.service';
 
 @Component({
-  moduleId: module.id,
-  selector: 'site-nav',
-  templateUrl: './nav.component.html'
+  selector: 'profile',
+  templateUrl: 'profile.component.html'
 })
-export class NavComponent implements OnInit {
+export class ProfileComponent implements OnInit {
+  
   constructor(private auth:AuthService) {
     auth.handleAuthentication();
   }
 
   ngOnInit() {
   }
-  
 }
