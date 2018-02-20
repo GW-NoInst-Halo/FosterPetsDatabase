9d92bce56b9077a1b2cec7a8479ca099e84edaac
Andres <andresreyes12131997@gmail.com>
2018-02-19 21:01:50 -0800

    add about page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RoutingModule } from './app.routes';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { AboutComponent } from './components/about/about.Component';
 import { PetBioComponent } from './components/pet-bio/pet-bio.component';
 import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome';
 import { AUTH_PROVIDERS } from 'angular2-jwt';
 import { AuthService } from './services/auth.service';
 
+
 @NgModule({
   declarations: [
     AppComponent,
-    PetBioComponent
+    NavComponent,
+    HomeComponent,
+    ProfileComponent,
+    PetBioComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
