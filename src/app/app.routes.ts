39c52dccb6d976e1f7963769f7e9ff0f4759bc85
Hiroyuki Xiong <xionghiroyuki@gmail.com>
2018-02-19 18:20:53 -0800

    Uploaded pictures into asset-image safe file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { PetBioComponent } from './components/pet-bio/pet-bio.component'
 
 import { ProfileComponent } from './components/profile/profile.component';
 
 const routes: Routes = [
   {
       path: '',
-      component: PetBioComponent
+      component: HomeComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
   },
 
 ];
